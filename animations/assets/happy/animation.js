d114f97d70d08050c6741e5fc036f6349b604652
thanks-cohn <officialmahasbiz@gmail.com>
2026-08-25 21:18:17 -0500

    feat: add happy Chutty animation

---
+++ animations/assets/happy/animation.js
@@ -0,0 +1,11 @@
+export default {
+  id: "happy",
+  name: "Happy",
+  trigger: "click",
+  cycle: true,
+  interruptible: true,
+  loop: false,
+  sequence: [
+    { face: ">ᴗ<", label: "YAY", className: "play-pop", duration: 340 }
+  ]
+};
