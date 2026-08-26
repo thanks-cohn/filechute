b86511c0188eb30e799269a468969307e4acd993
thanks-cohn <officialmahasbiz@gmail.com>
2026-08-25 21:18:23 -0500

    feat: add curious Chutty animation

---
+++ animations/assets/curious/animation.js
@@ -0,0 +1,11 @@
+export default {
+  id: "curious",
+  name: "Curious",
+  trigger: "click",
+  cycle: true,
+  interruptible: true,
+  loop: false,
+  sequence: [
+    { face: "•⤙•", label: "HMM", className: "play-wiggle", duration: 420 }
+  ]
+};
