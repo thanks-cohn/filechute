b83ddc15bec520fad015149294542bfec6055159
thanks-cohn <officialmahasbiz@gmail.com>
2026-08-25 21:18:36 -0500

    feat: add Chutty chomp animation

---
+++ animations/assets/chomp/animation.js
@@ -0,0 +1,11 @@
+export default {
+  id: "chomp",
+  name: "Chomp",
+  trigger: "click",
+  cycle: true,
+  interruptible: true,
+  loop: false,
+  sequence: [
+    { face: "•O•", label: "NOM?", className: "play-chomp", duration: 420 }
+  ]
+};
