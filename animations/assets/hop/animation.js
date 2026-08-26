f3c69526f57ca0ef62d7866991a507a45f919875
thanks-cohn <officialmahasbiz@gmail.com>
2026-08-25 21:18:30 -0500

    feat: add hopping Chutty animation

---
+++ animations/assets/hop/animation.js
@@ -0,0 +1,11 @@
+export default {
+  id: "hop",
+  name: "Hop",
+  trigger: "click",
+  cycle: true,
+  interruptible: true,
+  loop: false,
+  sequence: [
+    { face: "◕ᴗ◕", label: "AGAIN", className: "play-hop", duration: 460 }
+  ]
+};
